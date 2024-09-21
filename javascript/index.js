536ccd4c5c799ff964cd003f3671e3fd4ab1ecb7
Ahiati Emmanuel <ahiatiemmanuel77@gmail.com>
2024-09-21 23:18:52 +0000

    justdeylearn

---
+++ javascript/index.js
@@ -0,0 +1,14 @@
+
+
+let input = +prompt('Enter your number to check whether it is even or not?');
+let answer = '';
+if (input % 2 === 0){
+ answer === 'is an even number'
+};
+else if (input % 2 != 0){
+  answer === ' is an odd number.'
+};
+else{
+  answer ==='You entered a negative number. Try to enter a positive number instead.'
+};
+alert(`The number ${input} answer`);
